4c1b334931b3e19bd138eefb97692d92f41096c0
omardevm <omardguez.contacto@gmail.com>
2021-05-05 13:39:54 -0500

    Base Project

---
+++ app/src/main/java/com/omar/mvvmlogin/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.omar.mvvmlogin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
---
+++ app/src/main/java/com/omar/mvvmlogin/RegisterFragment.kt
@@ -0,0 +1,11 @@
+package com.omar.mvvmlogin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+}
